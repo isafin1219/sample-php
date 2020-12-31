6b5ac8566e571e757e1f5afdabeb480826156b30
Ilnur Safin <isafin1219@gmail.com>
2020-12-31 18:11:06 +0400

    31DEC2020 1811

--- sample.php
+++ sample.php
@@ -1,20 +1,11 @@
 <?php
 
 function normalizeUrl($url)
-{    
-    //$abc = '';
+{        
     if (strpos($url, 'http://') === 0) {
         $domain = substr($url, 7);
     } else {
         $domain = $url;
-    }
-
-    if(isset($domain)) {
-        echo "domain - Yes\n";        
-    }
-
-    if(isset($abc)) {
-        echo "abc - No";
     }
 
     return "https://{$domain}";
